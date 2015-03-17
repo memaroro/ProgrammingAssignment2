0b20d0300a44d145e66cfe69b47cd95938b313a0
memaroro <martabassols@gmail.com>
2015-03-17 15:02:10 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,18 @@
-##This function cache the matrix that you had passed it.
-##The function makes a list of 4 variables, which you will use in the next function.
+##THIS FUNCTION CREATES A SPECIAL MATRIX (makeCacheMatrix)
+##and makes a list of 4 variables, which you will use in the next function.
+##
+##The Set function is setting the cached numeric vector and is setting the cached "m" to NULL
+##The get function is returning the cached numeric vector
+##The setsolve requires a "special" matrix that you can compute its inverse(solve function)
+##              and will set the cache matrix
+##The getsolve returns the inverse of the cache matrix
 
 makeCacheMatrix <- function(x = numeric()){
   m <- NULL
 
   set <- function(y){
         x <<- y
-        m <- NULL
+        m <<- NULL
   }
   
   get <- function()x
@@ -19,12 +25,16 @@ makeCacheMatrix <- function(x = numeric()){
   
 } 
 
-
-## This function computes de inverse of the cache matrix. The solve function is for calculate the inverse of
-## a square matrix.
+## And with the cacheSolve function you will have the inverse of the cache matrix.
+## Calculates the inverse of the matrix(with the solve function) obtained from the makeCacheMatrix object.
+##First you will get the cache inverse of the matrix with "x$getsolve()" and assign it locally into "m"
+##if "m" is not NULL will return the cache inverse obtained from the makeCaceMatrix function.
+##if "m" is NULL then you get the cache matrix obtained from the makeCaceMatrix function 
+##   and assign it to the data variable
+##Finally calculates the inverse of the data variable and returns this calculated inverse
 
 cacheSolve <- function(x, ...){
-         m <- x$getsolve()
+         m <- x$getsolve() 
          if (!is.null(m)){
            message("getting cached data")
            return(m)
